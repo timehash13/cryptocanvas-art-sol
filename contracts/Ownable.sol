80bc03aedce6d447f6a9ec83fbb9181f5df07dc0
timehash13 <victory.dev523@gmail.com>
2026-05-08 11:08:34 +0900

    feat: complete rewardablecanvas, testableart contractws

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -37,5 +37,14 @@ contract Ownable {
         emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
     }
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
 
 }
